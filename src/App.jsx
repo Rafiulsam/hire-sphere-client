4fc59cbe22c892c8309348e70c67e2957d6180a7
Rafiul Sam <rafiulsam69@gmail.com>
2025-12-28 04:08:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 >this is job portal website </h1>
+     <button className="btn btn-soft btn-error">Error</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Register.jsx
@@ -0,0 +1,40 @@
+import Lottie from 'lottie-react';
+import registerAnimation from '../assets/register.json'
+
+const Register = () => {
+    const handleRegister = (e) => {
+        e.preventDefault()
+        const form = e.target
+        const email = form.email.value
+        const password = form.password.value
+        console.log(email, password);
+
+    }
+
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse gap-10">
+                <div className="text-center lg:text-left">
+                    <Lottie animationData={registerAnimation} loop={true}></Lottie>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <div className="card-body">
+                        <h1 className="text-5xl font-bold mb-4">Register now!</h1>
+                        <form onSubmit={handleRegister}>
+                            <fieldset className="fieldset">
+                                <label className="label">Email</label>
+                                <input name='email' type="email" className="input" placeholder="Email" />
+                                <label className="label">Password</label>
+                                <input name='password' type="password" className="input" placeholder="Password" />
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <button className="btn btn-neutral mt-4">Register</button>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
